1a28b7ef9558865c6162a0071f45521fbd92b179
whopperjames95 <james.goff1030@gmail.com>
2022-06-14 22:19:45 -0700

    assigment finished

---
+++ budget/BudgetList.py
@@ -0,0 +1,100 @@
+# from . import Expense
+
+# expenses = Expense.Expenses()
+# expenses.read_expenses('data/spending_data.csv')
+
+
+# # for expense in expenses.list:
+#     # expense.amount.append(myBudgetList)
+
+
+# class BudgetList():
+#     def __init__(self, budget):
+#         self.budget = budget
+#         self.sum_expenses = 0
+#         self.expenses = []
+#         self.sum_overages = 0
+#         self.overages = []
+
+#     def __append__(self, item):
+        
+#         if self.sum_expenses + item <= self.budget:
+#             self.expenses.append(item)
+#             self.sum_expenses += item
+#         else:
+#             self.overages.append(item)
+#             self.sum_overages += item
+
+#     def __len__(self):
+#         return len(self.expenses) + len(self.overages)
+
+#     def main():
+#         myBudgetList = BudgetList(1200)
+#         for expense in expenses.list:
+#             myBudgetList.append(expense.amount)
+#         print('The count of all expenses: ' + str(len(myBudgetList)))
+
+#     if __name__ == "__main__":
+#         main()
+
+
+
+
+
+
+from . import Expense
+import matplotlib.pyplot as plt
+
+class BudgetList():
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0 
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def append(self, item):
+        if((self.sum_expenses + item) < self.budget):
+            self.expenses.append(item)
+            self.sum_expenses += item
+        else:
+            self.overages.append(item)
+            self.sum_overages += item
+
+    def __len__(self):
+        return len(self.expenses) + len(self.overages)
+
+
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+
+
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses("data/spending_data.csv")
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+
+    for entry in myBudgetList:
+        print(entry)
+
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs total budget')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ budget/ExpenseCategories.py
@@ -0,0 +1,53 @@
+from . import Expense
+import matplotlib.pyplot as plt
+import timeit
+
+
+
+def main():
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+    divided_for_loop = expenses.categorize_for_loop()
+    divided_set_comp = expenses.categorize_set_comprehension()
+
+    if not divided_set_comp == divided_for_loop:
+        print("sets are not equal by == test")
+
+    for a, b in zip(divided_for_loop, divided_set_comp):
+        if not (a.issubset(b) and b.issubset(a)):
+            print("sets are not equal by subset test")
+
+    print(timeit.timeit(stmt='expenses.categorize_for_loop()',
+    setup =
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''',
+    number=100000,
+    globals=globals()
+    ))
+
+    print(timeit.timeit(stmt='expenses.categorize_set_comprehension()',
+    setup =
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''',
+    number=100000,
+    globals=globals()
+    ))
+
+    fix, ax = plt.subplots()
+    labels = ['Necessary', 'food', 'Unecessary']
+    divided_expenses_sum = []
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum(x.amount for x in category_exps ))
+
+    ax.pie(divided_expenses_sum, labels = labels, autopct='%1.1f%%')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
